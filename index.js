5c6d6ebe3a7e52ce1a60232401932acbb9a65437
Het Patel <137598780+Het2604@users.noreply.github.com>
2023-09-02 12:47:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+function red()
+{
+    document.getElementById("yyy").style.backgroundColor="#e60000";
+    document.getElementById("yy").style.backgroundColor="red";
+    document.getElementById("y").style.backgroundColor="red";
+}
+function orange()
+{
+    document.getElementById("yyy").style.backgroundColor="#e69500";
+    document.getElementById("yy").style.backgroundColor="orange";
+    document.getElementById("y").style.backgroundColor="orange";
+}
+
+function yellow()
+{
+    document.getElementById("yyy").style.backgroundColor="#ffff1a";
+    document.getElementById("yy").style.backgroundColor="yellow";
+    document.getElementById("y").style.backgroundColor="yellow";
+}
+
+function green()
+{
+    document.getElementById("yyy").style.backgroundColor="#00e600";
+    document.getElementById("yy").style.backgroundColor="green";
+    document.getElementById("y").style.backgroundColor="green";
+}
+
+function blue()
+{
+    document.getElementById("y").style.backgroundColor="#0000e6";
+    document.getElementById("yy").style.backgroundColor="blue";
+    document.getElementById("yyy").style.backgroundColor="blue";
+}
+function indigo()
+{
+    document.getElementById("yyy").style.backgroundColor="#590099";
+    document.getElementById("yy").style.backgroundColor="indigo";
+    document.getElementById("y").style.backgroundColor="indigo";
+}
+
+function violet()
+{
+    document.getElementById("yyy").style.backgroundColor="#ed78ed";
+    document.getElementById("yy").style.backgroundColor="violet";
+    document.getElementById("y").style.backgroundColor="violet";
+}
